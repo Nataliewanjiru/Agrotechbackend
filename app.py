792750f615e2c00e2d9d3c081e818567ae761d38
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-10-06 02:05:36 +0300

    New commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, request, jsonify
+from flask import Flask, redirect, render_template, request, jsonify
 import os
 
 
@@ -16,7 +16,7 @@ load_dotenv()
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("dburl")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 CORS(app)
